45a27364dbe991195fc2461abda7e46f2e664e74
Owen Luts <oluts47@gmail.com>
2023-11-08 12:52:50 -0500

    new file:   about_us/index.html 	new file:   concept_ideas/click_on_face.png 	new file:   concept_ideas/landing_page.png 	new file:   css/global.css 	new file:   css/styles.css 	new file:   data.json 	new file:   images/amy-897988508948500503/amy1.jpg 	new file:   images/clark-1124053366620823732/clark1.jpg 	new file:   images/clark-1124053366620823732/clark2.jpg 	new file:   images/clark-1124053366620823732/clark3.jpg 	new file:   images/dante-441708884378517514/dante1.jpg 	new file:   images/dante-441708884378517514/dante2.jpg 	new file:   images/dante-441708884378517514/dante3.jpg 	new file:   images/dj-704565290951901266/dj1.jpg 	new file:   images/dj-704565290951901266/dj2.jpg 	new file:   images/dj-704565290951901266/dj3.jpg 	new file:   images/gio-426074685340647424/gio1.jpg 	new file:   images/gio-426074685340647424/gio2.jpg 	new file:   images/gio-426074685340647424/gio3.jpg 	new file:   images/jack-214028075103551488/jack1.jpg 	new file:   images/jaysen-454722441780920330/jaysen1.jpg 	new file:   images/kiera-698766259243974716/kiera1.jpg 	new file:   images/kiera-698766259243974716/kiera2.jpg 	new file:   images/kiera-698766259243974716/kiera3.jpg 	new file:   images/mary_ann-395686692306157581/mary_ann1.jpg 	new file:   images/mary_ann-395686692306157581/mary_ann2.jpg 	new file:   images/mary_ann-395686692306157581/mary_ann3.jpg 	new file:   images/owen-703278178944417853/owen1.jpg 	new file:   images/owen-703278178944417853/owen2.jpg 	new file:   images/owen-703278178944417853/owen3.jpg 	new file:   images/parker-731634573317767275/parker1.jpg 	new file:   images/parker-731634573317767275/parker2.jpg 	new file:   images/parker-731634573317767275/parker3.jpg 	new file:   images/sam-526203167277318144/sam1.jpg 	new file:   images/sam-526203167277318144/sam2.jpg 	new file:   images/sam-526203167277318144/sam3.jpg 	new file:   images/yoav-674027373271973930/yoav1.jpg 	new file:   index.html 	new file:   resources/bubble-large.png 	new file:   resources/bubble-medium.png 	new file:   resources/bubble-small.png 	new file:   resources/ring.png 	new file:   resources/sams-club.png 	new file:   resources/sams-face.png 	new file:   resources/sign.jpg 	new file:   scripts/bubbles.js 	new file:   scripts/index.js

---
+++ scripts/bubbles.js
@@ -0,0 +1,35 @@
+const numBubbles = 50;
+const maxBubbleSize = 100;
+const windowHeight = document.getElementById('pages').offsetHeight;
+const windowWidth = document.getElementById('pages').offsetWidth;
+const bubbleHolder = document.getElementById('bubbles');
+
+function generateBubble() {
+    const bubble = document.createElement('img');
+    bubble.classList.add('bubble');
+    const bubbleWidth = Math.random() * maxBubbleSize
+    bubble.style.width = bubbleWidth + 'px';
+    bubble.style.top = (Math.random() * windowHeight) + 'px';
+    bubble.style.left = (Math.random() * windowWidth) +'px';
+    bubble.style.zIndex = -1 * width;
+    
+    bubble.src = 'resources/bubble-medium.png';
+
+    bubbleHolder.appendChild(bubble);
+}
+
+for (let i = 0; i < numBubbles; i++) {
+    generateBubble()
+}
+
+window.addEventListener('scroll', () => {
+    const distance = window.scrollY;
+    const constant = 0.005;
+    const bubbles = document.getElementsByClassName('bubble');
+
+    for (let bubble of bubbles) {
+        const magnification = bubble.offsetWidth;
+        bubble.style.transform = `translateY(${distance * Math.pow(magnification, 3/2) * constant * -1}px`;
+    }
+    
+});
